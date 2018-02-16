869ce214ee2ff8de8b20579e377c4d6125ad98f0
neerajpal09 <neerajpal09@gmail.com>
2018-02-16 17:43:07 +0530

    commented some code

--- sys/kern/sched_bsd.c
+++ sys/kern/sched_bsd.c
@@ -220,15 +220,19 @@ schedcpu(void *arg)
 	LIST_FOREACH(p, &allproc, p_list) {
 
 						/* below code to print process info */
+		/*
 		if (p->p_p->ps_pledge) {
 			
 			if ((p->p_p->ps_pledge & PLEDGE_STDIO) > 1) {
-				printf("total cpus: %d process prio: %5d cpuid: %5d pid: %10d pledge_xbit: %10llx io bound\n",ncpus,p->p_priority,p->p_cpu->ci_cpuid,p->p_p->ps_pid,p->p_p->ps_pledge);
+				printf("our_choice: %2d system_choice: %2d cpuid: %5d pid: %10d pledge_xbit: %10llx io bound\n",p->our_choice,p->choice,p->p_cpu->ci_cpuid,p->p_p->ps_pid,p->p_p->ps_pledge);
 			}
 			else {
 				printf("total cpus: %d process prio: %5d cpuid: %5d pid: %10d pledge_xbit: %10llx io bound\n",ncpus,p->p_priority,p->p_cpu->ci_cpuid,p->p_p->ps_pid,p->p_p->ps_pledge);
 			}
 		}
+
+	*/
+
 		/*
 		 * Increment sleep time (if sleeping). We ignore overflow.
 		 */
